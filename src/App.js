3dc0fe9a5d9d54a028516f6f6471e96ad28ace3c
Mainak Das <82697942+mainak15melody@users.noreply.github.com>
2022-09-02 22:19:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import ImageContainer from "./components/ImageSelectorAndContainer";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageContainer/>
+    </div>
+  );
+}
+
+export default App;
